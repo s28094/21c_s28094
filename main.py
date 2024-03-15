cd0ac00062b5df8e17cd05da7527530675272354
s28094 <s28094@pjwstk.edu.pl>
2024-03-15 16:20:09 +0100

    Done Task 3.
    Add class SquareGenerator
    Add function to SquareGenerator named e_squares(self, start, end)

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ start = 1
 end = 10
 result = e_squares(start, end)
 print(result)
+
+# Task 3
+
+class SquareGenerator:
+    def e_squares(self, start, end):
+        squares = [x ** 2 for x in range(start, end + 1)]
+        return squares
+
+
+start_num = 1
+end_num = 10
+squares_list = SquareGenerator().e_squares(start_num, end_num)
+print(squares_list)
