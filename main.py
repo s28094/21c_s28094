4ea4159dfbe21dc806ccb69e01273129be7018f1
s28094 <s28094@pjwstk.edu.pl>
2024-03-15 16:14:57 +0100

    Done Task 2.
    Add new function e_squares(start, end)

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 
 squares = [x ** 2 for x in range(1, 11)]
 print(squares)
+
+# Task 2
+
+def e_squares(start, end):
+    squares = [x**2 for x in range(start, end + 1)]
+    return squares
+
+start = 1
+end = 10
+result = e_squares(start, end)
+print(result)
