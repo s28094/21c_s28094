3e73e58f75b0d55c714b05a12fd136d5fcb70625
s28094 <s28094@pjwstk.edu.pl>
2024-03-15 16:21:14 +0100

    Comment Tasks 1,2 and 3

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+
+"""
 # Task 1
 
 squares = [x ** 2 for x in range(1, 11)]
 print(squares)
-
+"""
 # Task 2
-
+"""
 def e_squares(start, end):
     squares = [x**2 for x in range(start, end + 1)]
     return squares
@@ -13,9 +15,9 @@ start = 1
 end = 10
 result = e_squares(start, end)
 print(result)
-
+"""
 # Task 3
-
+"""
 class SquareGenerator:
     def e_squares(self, start, end):
         squares = [x ** 2 for x in range(start, end + 1)]
@@ -26,3 +28,6 @@ start_num = 1
 end_num = 10
 squares_list = SquareGenerator().e_squares(start_num, end_num)
 print(squares_list)
+"""
+# Task 4
+
